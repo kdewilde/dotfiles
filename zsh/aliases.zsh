67eb28155360b84211ca502030ed766c3deefd95
Kevin De Wilde <kevin.dewilde.ext@seeds-excellence.com>
2019-01-18 14:42:52 +0100

    Initial commit / cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,26 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias open="xdg-open"
+alias ll="ls -lhA"
+
+alias cdc="cd ~/myprojects/ceres"
+alias cdcf="cd ~/myprojects/ceres/ceres-web/ceres-web-fe/integration/WAR/src/main/webapp/js/ext6"
+alias runc="cd ~/myprojects/ceres/ceres-application-starter/mvnscripts && ./buildAndRunCeres.sh"
+alias watchc="cd ~/myprojects/ceres && lsyncd lsyncd.lua"
+alias codec="code ~/myprojects/ceres/ceres-web/ceres-web-fe/integration/WAR/src/main/webapp/js/ext6"
+
+alias runvp="cd ~/myprojects/vectoring-platform/vectoring-platform/integration/war && mvn cargo:run -o -Pdevelopment"
+alias cdvp="cd ~/myprojects/vectoring-platform"
+alias watchvp="cd ~/myprojects/vectoring-platform && lsyncd lsyncd.lua"
+alias codevp="code ~/myprojects/vectoring-platform/vectoring-platform/integration/war/src/main/webapp"
+
+alias runv="cd ~/myprojects/vesta/rf-integration/integration-war && mvn cargo:run -o"
+alias cdv="cd ~/myprojects/vesta"
+alias watchv="cd ~/myprojects/vesta && lsyncd lsyncd.lua"
+alias codev="code ~/myprojects/vesta/rf-integration/integration-war/src/main/webapp"
+
+alias vimrc="vi ~/.vimrc"
+alias zshrc="vi ~/.zshrc"
+alias i3conf="vi ~/.config/i3/config"
