edaba72a52054fe1018836409a6552cf77c5e0d1
kdewilde <kevin@dewil.de>
2019-01-19 16:43:55 +0100

    install script no longer needed. symlink now takes folder path into account

--- vscode/install.sh
+++
@@ -1,7 +0,0 @@
-# Symlink vscode user settings
-
-CURRENT_DIR="$(dirname "$0")"
-SETTINGS_DIR="$HOME/.config/Code/User"
-
-ln -s "$CURRENT_DIR"/settings.json "$SETTINGS_DIR"/settings.json
-ln -s "$CURRENT_DIR"/keybindings.json "$SETTINGS_DIR"/keybindings.json
