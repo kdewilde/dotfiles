b6acc248e4d05a943d30b2416e02d259cb1b7b92
Kevin De Wilde <kevin@dewil.de>
2020-03-19 17:09:25 +0100

    introduced env variables for paths and use them in ranger

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,7 @@
 export EDITOR='vim'
-export BROWSER='chromium termite'
+export BROWSER='chromium'
+export MEMO_HOME=$HOME"/memo"
+export DOT_HOME=$HOME"/dotfiles"
+export PROJECTS_HOME=$HOME"/Projects"
+export QMK_HOME=$HOME"/Documents/qmk_keyboards"
+export SHARE_HOME=$HOME"/share"
