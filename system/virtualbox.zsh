933d14c6961fcf55fe1b6627612628ead8d17cfa
Kevin De Wilde <kevin@dewil.de>
2020-03-19 16:56:33 +0100

    fixed automatic virtualbox mounting

---
+++ system/virtualbox.zsh
@@ -0,0 +1,9 @@
+# this will mount a virtualbox folder named share to the SHARE_HOME env variable
+
+if [[ ! $( grep $SHARE_HOME" " /proc/mounts ) ]] 
+then
+    echo "Mounting Virtualbox shared folder under $SHARE_HOME"
+    mkdir -p $SHARE_HOME
+    sudo mount -t vboxsf share $SHARE_HOME
+fi
+
