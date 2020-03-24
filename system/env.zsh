a9018356d62c8934b471faf0720c486c0243b686
kdewilde <kevin@dewil.de>
2020-03-24 13:10:42 +0100

    updated $BROWSER dependent on installed browser

--- system/env.zsh
+++ system/env.zsh
@@ -1,7 +1,14 @@
 export EDITOR='vim'
-export BROWSER='chromium'
+
 export MEMO_HOME=$HOME"/memo"
 export DOT_HOME=$HOME"/dotfiles"
 export PROJECTS_HOME=$HOME"/Projects"
 export QMK_HOME=$HOME"/Documents/qmk_keyboards"
 export SHARE_HOME=$HOME"/share"
+
+if hash google-chrome 2>/dev/null; then
+    export BROWSER='google-chrome'
+fi
+if hash chromium 2>/dev/null; then
+    export BROWSER='chromium'
+fi
