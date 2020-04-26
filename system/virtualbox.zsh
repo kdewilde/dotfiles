560b85aa25c5d9e3a7a9a0b8e12a5a7b254a93a0
Kevin De Wilde <kevin@dewil.de>
2020-04-26 15:46:00 +0200

    virtualbox share name is giving problems, renamed to virtualboxShare

--- system/virtualbox.zsh
+++ system/virtualbox.zsh
@@ -4,6 +4,6 @@ if [ -d $SHARE_HOME ] && [[ ! $( grep $SHARE_HOME" " /proc/mounts ) ]]
 then
     echo "Mounting Virtualbox shared folder under $SHARE_HOME"
     mkdir -p $SHARE_HOME
-    sudo mount -t vboxsf share $SHARE_HOME
+    sudo mount -t vboxsf virtualboxShare $SHARE_HOME
 fi
 
