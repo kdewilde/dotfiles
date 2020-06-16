384b9b62f7da4f7545039e9d99e52b5ac8ef9278
kdewilde <kevin@dewil.de>
2020-06-16 08:46:47 +0200

    updated env variables for work en shared folder

--- system/env.zsh
+++ system/env.zsh
@@ -4,7 +4,13 @@ export MEMO_HOME=$HOME"/memo"
 export DOT_HOME=$HOME"/dotfiles"
 export PROJECTS_HOME=$HOME"/Projects"
 export QMK_HOME=$HOME"/Documents/qmk_keyboards"
-export SHARE_HOME=$HOME"/share"
+if [ -d $HOME"/share" ]; then 
+    export SHARE_HOME=$HOME"/share"
+    export VIRTUALBOX_HOME=$HOME"/share"
+fi
+if [ -d $HOME"/shared-drives/C:/Users/dewildke/Downloads" ]; then
+    export SHARE_HOME=$HOME"/shared-drives/C:/Users/dewildke/Downloads"
+fi
 
 if hash google-chrome 2>/dev/null; then
     export BROWSER='google-chrome'
